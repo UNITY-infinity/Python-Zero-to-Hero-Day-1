dcb45bdb1fce7ef0ac843448b1408a08647d748b
UNITY-infinity <81506136+UNITY-infinity@users.noreply.github.com>
2021-08-17 03:55:06 +0300

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,10 @@
+#Takes two inputs,
+#stores in variable x and y, 
+#and prints the result of x to the power y
+
+#input x and y
+x = int(input("Enter a value as base: "))
+y = int(input("Enter a value as power to which the base will be raised: "))
+
+#print x to the power of y
+print(x,' to the power of ',y,' is equal to: ',x**y)
